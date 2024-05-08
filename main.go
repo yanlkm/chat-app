ce969fb7a5a8ae4110d1fafe72b3987024bbd3f0
@yanlkm <yannmarcel43@gmail.com>
2024-05-08 03:49:09 +0200

    deploy: minor update port in main

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 	// Start HTTP server
 	port := os.Getenv("PORT")
 	fmt.Printf("Server started on %s", port)
-	err = http.ListenAndServe(os.Getenv("BASE_URL"), r)
+	err = http.ListenAndServe("localhost:"+port, r)
 	if err != nil {
 		fmt.Println("Failed to start server: %v", err)
 	}
