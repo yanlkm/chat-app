09f5fbe62347d7f8877fc4cf583e988c51066940
@yanlkm <yannmarcel43@gmail.com>
2024-05-30 23:04:55 +0200

    feat : add more verifications in api requests globally

--- pkg/middlewares/auth.go
+++ pkg/middlewares/auth.go
@@ -49,3 +49,72 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+// Check If User Is Logged In and If Is Admin
+func IsAdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get token from cookie
+		var token string
+		token, err := c.Cookie("token")
+		if err != nil {
+			token = c.GetHeader("Authorization")
+			if token == "" {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "1 - Unauthorized"})
+				c.Abort()
+				return
+			}
+		}
+
+		// Verify and decode token
+		claims, err := utils.VerifyToken(&token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "2 - Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		//check if user is admin
+		if claims.Role != "admin" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "3 - Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.UserID)
+		c.Set("role", claims.Role)
+
+		// Continue with the request
+		c.Next()
+
+	}
+}
+
+// Check If User Is Logged In
+func IsLoggedInMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get token from cookie
+		var token string
+		token, err := c.Cookie("token")
+		if err != nil {
+			token = c.GetHeader("Authorization")
+			if token == "" {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "1 - Unauthorized"})
+				c.Abort()
+				return
+			}
+		}
+
+		// Verify and decode token
+		claims, err := utils.VerifyToken(&token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "2 - Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.UserID)
+
+		// Continue with the request
+		c.Next()
+	}
+}
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -40,34 +40,34 @@ func NewRouter(userService user.UserService, codeService code.CodeService, authS
 		user.DeleteUserHandler(userService))
 
 	//code route
-	r.POST("codes",
+	r.POST("codes", middlewares.IsAdminMiddleware(),
 		code.CreateCodeHandler(codeService))
 
 	// Room routes
 	r.GET("rooms",
 		room.GetRoomsHandler(roomService))
-	r.GET("rooms/:id",
+	r.GET("rooms/:id", middlewares.IsLoggedInMiddleware(),
 		room.GetRoomHandler(roomService))
-	r.GET("rooms/user/:id",
+	r.GET("rooms/user/:id", middlewares.IsLoggedInMiddleware(),
 		room.GetUserRoomsHandler(roomService))
-	r.POST("rooms",
+	r.POST("rooms", middlewares.IsAdminMiddleware(),
 		room.CreateRoomHandler(roomService))
-	r.PUT("rooms/add/:id",
+	r.PUT("rooms/add/:id", middlewares.IsLoggedInMiddleware(),
 		room.AddMemberToRoom(roomService))
-	r.PUT("rooms/remove/:id",
+	r.PUT("rooms/remove/:id", middlewares.IsLoggedInMiddleware(),
 		room.RemoveMemberFromRoom(roomService))
 	// get all members of a room
-	r.GET("rooms/members/:id",
+	r.GET("rooms/members/:id", middlewares.IsLoggedInMiddleware(),
 		room.GetRoomMembersHandler(roomService, userService))
-	r.DELETE("rooms/delete/:id",
+	r.DELETE("rooms/delete/:id", middlewares.IsAdminMiddleware(),
 		room.DeleteRoomHandler(roomService))
 
 	// Message routes
-	r.POST("messages",
+	r.POST("messages", middlewares.IsLoggedInMiddleware(),
 		message.CreateMessageHandler(messageService))
-	r.GET("messages/:id",
+	r.GET("messages/:id", middlewares.IsLoggedInMiddleware(),
 		message.GetMessagesHandler(messageService))
-	r.DELETE("messages/:id",
+	r.DELETE("messages/:id", middlewares.IsLoggedInMiddleware(),
 		message.DeleteMessageHandler(messageService))
 
 	// auth routes
